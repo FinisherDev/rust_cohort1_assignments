61a8e2d4aa3b01fb11ac8e88cc92189ce9424492
FinisherDev <molokwujude002@gmail.com>
2025-03-31 15:13:18 +0100

    GPA claculator added

--- assignment4/src/main.rs
+++ assignment4/src/main.rs
@@ -6,7 +6,6 @@ const OPERATORS: [char; 5] = [
     '~'
 ];
 
-
 fn main() {
     print!("
     --------The Simple Rust Calculator------------
@@ -23,9 +22,21 @@ fn main() {
     ");
     println!("With that said...\n");
 
-    loop {
+    loop { 
+        fn end_function() -> String{
+            println!("\nWould you like to go on? (y/n)");
+            let mut input = String::new();
+            let _ = std::io::stdin().read_line(&mut input);
+            match input.trim().parse::<String>() {
+                Ok(ans) => {
+                   return ans;
+                }
+            }
+        }
+
         println!("Enter the first number (or ask for help)");
         let num_1 = input_validator();
+
 
         println!("Enter the operation");
         let operation = operator();
@@ -36,22 +47,16 @@ fn main() {
         let result = operation_handler(operation, num_1, num_2);
         print!("This is the result of the operation: {}", result);
 
-        println!("Would you like to go on? (y/n)");
-        let mut input = String::new();
-        let _ = std::io::stdin().read_line(&mut input);
-        match input.trim().parse::<String>() {
-            Ok(ans) => {
-                if ans == "n" {
-                    break
-                } else {
-                    continue
-                }
-            }
+        let answer = end_function();
+        if answer == "n" {
+            break;
+        } else {
+            continue;
         }
     }
 }
 
-fn input_validator() -> (String, f32){
+fn input_validator() -> (&'static str, f32){
     let mut input = String::new();
     let _ = std::io::stdin().read_line(&mut input);
     match input.trim().parse::<String>() {
@@ -78,22 +83,21 @@ fn input_validator() -> (String, f32){
                 |   entered, but it is not    |
                 |   used.                     |
                 -------------------------------
-                | 'cgpa' Starts cgpa          |
-                |        calculator.Useful    |
-                |        for students.        |
-                |        (Coming Soon)        |
+                | 'cgpa' Starts GPA calculator|
+                |        Useful for students. |
                 | 'help' Displays this page   |
                 -------------------------------
                 ");
                 println!("\nEnter the first number(or what ever other operation)");
                 input_validator();
-                return ("".to_string(), f32::NAN);
+                return ("", f32::NAN);
             } else if input == "cgpa" {
                 println!("Preparing calculator...");
-                return ("".to_string(), f32::NAN);
+                gpa_calculator();
+                return  ("cgpa", f32::NAN);
             } else {
                 let num:f32 = input.parse().expect("This one no dey");
-                return ("".to_string(), num);
+                return ("", num);
             }
         }
     }
@@ -113,7 +117,7 @@ fn operator() -> char{
     }
 }
 
-fn operation_handler(op: char, num1:(String, f32), num2:(String, f32)) -> f32{
+fn operation_handler(op: char, num1:(&str, f32), num2:(&str, f32)) -> f32{
     match op {
         '+' => return num1.1 + num2.1,
         '-' => return num1.1 - num2.1,
@@ -124,3 +128,61 @@ fn operation_handler(op: char, num1:(String, f32), num2:(String, f32)) -> f32{
     }
 }
 
+fn gpa_calculator() {
+    let mut total_credits: u16 = 0;
+    let mut course_point_avg: u16 = 0;
+    let mut credits: Vec<u16> = vec![0; 0];
+    let mut grades: Vec<char> = vec!['.'; 0];
+    let mut grade_points: Vec<u16> = vec![0; 0];
+    let  course_num: u16;
+    let  gpa:f32;
+
+    println!("\nEnter the number of courses");
+    let mut input = String::new();
+    let _ = std::io::stdin().read_line(&mut input);
+    match input.trim().parse() {
+        Ok(n) => {
+            course_num = n;
+        },
+        Err(_) => todo!()
+    }
+
+    println! ("\nEnter the credit loads of your courses");
+    for _i in 0..course_num {
+        let mut input = String::new();
+        let _ = std::io::stdin().read_line(&mut input);
+        let credit:u16 = input.trim().parse().expect("G");
+        credits.push(credit);
+        println!("{:?}", credits);
+    }
+
+    println! ("\nEnter your grades using capital letters");
+    for _i in 0..course_num {
+        let mut input = String::new();
+        let _ = std::io::stdin().read_line(&mut input);
+        let x = input.trim();
+        let grade = x.chars().next().expect("Empty");
+        grades.push(grade);
+        println!("{:?}", grades);
+    }
+
+    for i in grades {
+        match i {
+            'A'=> grade_points.push(5),
+            'B' => grade_points.push(4),
+            'C' => grade_points.push(3),
+            'D' => grade_points.push(2),
+            'E' => grade_points.push(1),
+            'F' => grade_points.push(0),
+            _ => todo!(),
+        }
+    }
+
+    for i in 0..grade_points.len() {
+        total_credits += credits[i];
+        course_point_avg += grade_points[i] * credits[i];
+    }
+
+    gpa = course_point_avg as f32 / total_credits as f32;
+    println!("\n Your Grade Point Average  {}", gpa);
+}
